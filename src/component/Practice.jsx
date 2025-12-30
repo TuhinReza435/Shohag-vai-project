fd1ddb7e37b4c48a5fecd9ad9c4d66c12b420dda
Tuhin Reza <tuhinreza02042002@gmail.com>
2025-12-30 22:38:00 +0600

    event

--- src/component/Practice.jsx
+++ src/component/Practice.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-
 const Practice = () => {
-  const listitem = [
-    { id: 1, value: "Learn react Like a Cake Walk" },
-    { id: 2, value: "Get String in JavaScript" },
-    { id: 3, value: "Be Master of Problem solving" },
-  ];
-  const items = [];
-  listitem.map((element) => {
-    const val = <li key={element.id}>{element.value}</li>;
-    items.push(val)
-  });
-  return <div>{items}</div>
+  function handleClick() {
+    console.log("You Click me");
+  }
+  return (
+    <div>
+      <button
+        onClick={handleClick}
+        className="bg-amber-800 text-white p-1 rounded-2xl cursor-pointer"
+      >
+        Click me
+      </button>
+    </div>
+  );
 };
 
 export default Practice;
