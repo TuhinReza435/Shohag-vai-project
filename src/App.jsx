f456715c85d8f8fe1171afc2cf4a391d22a3ba59
Tuhin Reza <tuhinreza02042002@gmail.com>
2026-01-03 15:20:13 +0600

    user add and change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+
 import Practice from "./component/Practice";
 function App() {
+  
   return (
-    <div className="flex justify-center content-center h-80 bg-amber-100">
-     <Practice/>
+    <div className="flex justify-center items-center">
+      <Practice />
     </div>
   );
 }
